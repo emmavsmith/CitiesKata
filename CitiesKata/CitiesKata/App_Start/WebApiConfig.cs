2a8ee3326175ffb19b9542b0ac2fc302134b461c
Emma Smith <emmasmith059@gmail.com>
2016-07-26 21:02:29 +0100

    CityController with Get endpoint to get all seeded cities

--- CitiesKata/CitiesKata/App_Start/WebApiConfig.cs
+++ CitiesKata/CitiesKata/App_Start/WebApiConfig.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace CitiesKata
 {
@@ -10,6 +8,13 @@ namespace CitiesKata
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            // Use camel case for JSON data.
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ CitiesKata/CitiesKata/Controllers/CityController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using CitiesKata.EF.DbContext;
+
+namespace CitiesKata.Controllers
+{
+    public class CityController : ApiController
+    {
+        // Todo: Dependency Injection
+        private readonly CitiesKataContext _dbContext = new CitiesKataContext();
+
+        // GET api/city
+        public IHttpActionResult Get()
+        {
+            var response = new List<CityResponse>();
+            var cities = _dbContext.Cities.ToList();
+
+            foreach (var city in cities)
+            {
+                response.Add(new CityResponse(city.Name));
+            }
+
+            return this.Ok(response);
+        }
+    }
+
+    public class CityResponse
+    {
+        public CityResponse(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
